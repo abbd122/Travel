8732aa2631afc0108f57bf09408e6addbedc0535
wangzheng <446404815@qq.com>
2019-12-06 00:32:27 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// 1
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App'
 import router from './router'
-// 引入
 import fastClick from 'fastclick'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+// 2
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
-// 绑定
 fastClick.attach(document.body)
+// 3
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="wrapper">
+    <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+          id: '0001',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/f74957827d190508c2598fb79ec36102.jpg_750x200_2960f17f.jpg'
+      },{
+          id: '0002',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/c71ed2851f33290c81c9803f7518fa11.jpg_750x200_06e5342e.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    // >>>进行样式穿透，不受scoped限制
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 31.25%
+        background: #eee
+        .swiper-img
+            width: 100%
+</style>
